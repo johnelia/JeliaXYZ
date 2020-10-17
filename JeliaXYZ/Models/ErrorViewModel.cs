23bf52c06c205e17b91bede344f7b4c9aab458d1
Lovesmiles2 <joe9056@nyp.org>
2020-10-16 20:05:55 -0400

    Add project files.

---
+++ JeliaXYZ/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JeliaXYZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
